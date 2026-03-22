75cdbe143cae881cfea0aea48177ef4efb493d89
Luiz Fernando Mineiro <142529915+lfmineiro@users.noreply.github.com>
2026-03-22 13:16:47 -0300

    feat(routes): configurar novas rotas e redirecionamentos no App

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/auth/Login'
+import { Dashboard } from './pages/Dashboard'
+import { MainLayout } from './layouts/MainComponent'
 
 function App() {
 
@@ -8,7 +10,10 @@ function App() {
   <Routes> 
     <Route path='/' element={<Navigate to="/login" replace />} />
     <Route path='/login' element={<LoginPage />} />
-    
+
+    <Route element={<MainLayout />}>
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Route>
   </Routes>
   )
 }
