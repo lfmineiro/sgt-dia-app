818ec146e44a3dbb0c40d1789f2baae1a3a884b4
Luiz Fernando Mineiro <142529915+lfmineiro@users.noreply.github.com>
2026-03-22 16:13:35 -0300

    feat(header): recupera os dados da api e renderiza na página

--- frontend-app/src/components/layout/Header.tsx
+++ frontend-app/src/components/layout/Header.tsx
@@ -1,14 +1,34 @@
+import { buscarSargentoDeDia } from "../../services/alunos.service";
+import { useQuery } from '@tanstack/react-query';
+
 export const Header = () => {
-  // Mais tarde isso virá da API, por enquanto é estático igual seu esboço
-  const nomeSgtDia = "Al 4º Ano Fulano";
-  const dataHoje = "Segunda-Feira, 23 De Fevereiro De 2026";
+//  vamos puxar da API usando o react Query 
+  const { data: sgtDia, isLoading } = useQuery({
+    queryKey: ['sgtAtual'],
+    queryFn: buscarSargentoDeDia
+  })
+
+  // const nomeSgtDia = "Al 4º Ano Fulano";
+  const dataHoje = new Date().getFullYear();
+
+  // calcular a turma do elemento 
+  // const CalcularTurma = () => {
+  //   const dataHoje = new Date()
+  //   console.log(dataHoje)
+  //   return dataHoje
+  // }
 
   return (
     <header className="bg-white border-b border-slate-100 p-8 shadow-sm">
       <div className="flex items-center justify-between">
         <div>
           <h1 className="text-3xl font-extrabold text-slate-950">
-            Sgt Dia: <span className="text-blue-700">{nomeSgtDia}</span>
+            {isLoading ? 
+              <span>Carregando...</span> : 
+              <>
+                Sgt Dia: <span className="text-blue-700">Al {sgtDia?.numero} - {sgtDia?.nomeGuerra}</span>
+              </>
+              }
           </h1>
           <p className="mt-1 text-xl text-slate-600">
             {dataHoje}
