2358374668310a78251dc9e82a70bdb6767558df
Luiz Fernando Mineiro <142529915+lfmineiro@users.noreply.github.com>
2026-04-19 23:45:54 -0300

    feat(alteracao): criar função para atualizar o status

--- backend-app/src/services/alteracao.service.ts
+++ backend-app/src/services/alteracao.service.ts
@@ -27,3 +27,22 @@ export const listarAlteracoesAtuais = async (): Promise<Alteracao[]> => {
 
   return alteracoes
 }
+
+export const atualizarStatusAlteracao = async (
+  id: string,
+  status: Alteracao["status"],
+): Promise<Alteracao> => {
+  const alteracaoExistente = await prisma.alteracao.findUnique({
+    where: { id },
+    select: { id: true },
+  })
+
+  if (!alteracaoExistente) {
+    throw new Error("ALTERACAO_NAO_ENCONTRADA")
+  }
+
+  return await prisma.alteracao.update({
+    where: { id },
+    data: { status },
+  })
+}
