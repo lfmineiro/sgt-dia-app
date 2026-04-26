812599f7b481436d39e1003ec718e4bc90f16603
Luiz Fernando Mineiro <142529915+lfmineiro@users.noreply.github.com>
2026-04-26 15:57:50 -0300

    feat(servico): orquestra fechamento de servico e atualizacao de alteracoes

--- backend-app/src/services/servicos.service.ts
+++ backend-app/src/services/servicos.service.ts
@@ -4,19 +4,36 @@ import type { CriarServicoBody } from "../schemas/servicos.schema.js";
 export const criarNovoServico = async (input: CriarServicoBody ) => {
   const parsedDate = new Date(input.data ?? "")
 
-  const novoServico = await prisma.servico.create({
-        data: {
-          data: parsedDate,
-          membrosGuarnicao: {
-            create: input.membros.map((m) => ({
-              alunoNumero: m.alunoNumero,
-              funcao: m.funcao,
-            })),
+  const servicoCriado = await prisma.$transaction(async (tx) => {
+    
+    // Primeiro vamos fechar o serviço anterior
+    await tx.servico.updateMany({
+      where: { status: 'EM_ANDAMENTO' },
+      data: { status: 'FECHADO' }
+    })
+
+    // As alterações que estavam como novas, vão para pendentes
+    await tx.alteracao.updateMany({
+      where: { status: 'NOVA' },
+      data: { status: 'PENDENTE' }
+    })
+
+
+    const novoServico = await tx.servico.create({
+          data: {
+            data: parsedDate,
+            membrosGuarnicao: {
+              create: input.membros.map((m) => ({
+                alunoNumero: m.alunoNumero,
+                funcao: m.funcao,
+              })),
+            },
           },
-        },
-        include: {
-          membrosGuarnicao: true,
-        },
-      });
-      return novoServico
+          include: {
+            membrosGuarnicao: true,
+          },
+        });
+        return novoServico
+  }) 
+  return servicoCriado
 }
