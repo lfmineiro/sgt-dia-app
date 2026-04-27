13e9ef929202c4c6a60c19d7160a15b45d188922
Luiz Fernando Mineiro <142529915+lfmineiro@users.noreply.github.com>
2026-04-26 21:09:38 -0300

    feat(service): implementa função de busca corretamente e remove a temporária

--- frontend-app/src/services/alunos.service.ts
+++ frontend-app/src/services/alunos.service.ts
@@ -1,22 +1,6 @@
 import type { Aluno } from '../types/aluno.types';
 import { api } from './api';
 
-
-// Vamos pegar apenas o primeiro aluno da lista para testar
-export const buscarSargentoDeDia = async (): Promise<Aluno | null> => {
-  try {
-    const response = await api.get('/alunos');
-    if (response.data && response.data.length > 0) {
-      // console.log(response.data)
-      return response.data[0]; 
-    }
-    return null;
-  } catch (error) {
-    console.error("Erro ao buscar Sargento de Dia:", error);
-    return null;
-  }
-};
-
 export const buscarAlunos = async () => {
   const response = await api.get('/alunos');
   return response.data;
