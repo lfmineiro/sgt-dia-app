9ceca406e62368acf0554b468ffb508abf47aa33
Luiz Fernando Mineiro <142529915+lfmineiro@users.noreply.github.com>
2026-04-29 15:59:11 -0300

    refactor(mobile): remove arquivos default e cria tela inicial limpa

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,24 +1,31 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+    <>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#fff',
+          },
+          headerTintColor: '#000',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerShown: false, 
+        }}
+      >
+        <Stack.Screen 
+          name="index" 
+          options={{ 
+            title: 'Início' 
+          }} 
+        />
+        
       </Stack>
+
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
