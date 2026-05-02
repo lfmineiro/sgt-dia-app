bc74c359f6e15b46f7239d61cca2eaa78031918d
Luiz Fernando Mineiro <142529915+lfmineiro@users.noreply.github.com>
2026-05-02 12:58:28 -0300

    feat(mobile): integra dados da api na listagem dinamica de quartos

--- mobile-app/src/components/pages/Alteracoes/toggleQuartos.tsx
+++ mobile-app/src/components/pages/Alteracoes/toggleQuartos.tsx
@@ -1,22 +1,29 @@
 import { getSetorByAba, MAPEAMENTO_QUARTOS } from "@/src/constants/locais"
 import { ComodoCard } from "./comodoCard"
+import type { Alteracao } from "@/src/types/alteracao.types"
 
 type ToggleQuartosProps = {
   abaAtiva: string
+  alteracoes: Alteracao[] | null
 }
 
-export const ToggleQuartos = ({ abaAtiva }: ToggleQuartosProps) => {
+export const ToggleQuartos = ({ abaAtiva, alteracoes }: ToggleQuartosProps) => {
   const setorChave = getSetorByAba(abaAtiva)
 
   const comodos = MAPEAMENTO_QUARTOS[setorChave]
 
+  
   return (
     comodos.map((comodo) => {
+      const temAlteracaoPendente =  alteracoes?.some(
+        alt => alt.comodo === comodo.id 
+      )
+      const statusLabel = temAlteracaoPendente ? "Pendente" : "Verificado"
       return (
         <ComodoCard
         key={comodo.id}
         nomeComodo={comodo.label}
-        status="Pendente"
+        status={statusLabel}
         onPress={() => console.log('Vai abrir o toggle')}
         /> 
 
