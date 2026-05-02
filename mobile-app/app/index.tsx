97259d93c883f5db1fbc9e9abcc0dccf0ceba9cb
Luiz Fernando Mineiro <142529915+lfmineiro@users.noreply.github.com>
2026-05-02 12:58:28 -0300

    feat(mobile): integra dados da api na listagem dinamica de quartos

--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -2,12 +2,15 @@ import { TabsHeader } from '@/src/components/layout/TabsHeader';
 import { TopBar } from '@/src/components/layout/TopBar';
 import { ToggleQuartos } from '@/src/components/pages/Alteracoes/toggleQuartos';
 import { ABAS_ALTERACOES } from '@/src/constants/locais';
+import { useAlteracoes } from '@/src/hooks/useAlteracoes';
 import { useState } from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
 
 export default function HomeScreen() {
   const [abaAtiva, setAbaAtiva] = useState(ABAS_ALTERACOES[0])
   
+  const { alteracoes } = useAlteracoes()
+
   return (
     <View style={styles.container}>
       <TopBar userNameInitials='LF'/>
@@ -18,6 +21,7 @@ export default function HomeScreen() {
       <ScrollView style={styles.listaQuartos}>
         <ToggleQuartos 
         abaAtiva={abaAtiva}
+        alteracoes={alteracoes}
         />
         
       </ScrollView>
