928ca1fc756e6f9282e4cfa74f05bc3da096096b
Luiz Fernando Mineiro <142529915+lfmineiro@users.noreply.github.com>
2026-05-03 12:50:35 -0300

    chore(api): adiciona um log para a resposta da requisicao

--- mobile-app/src/services/api.ts
+++ mobile-app/src/services/api.ts
@@ -11,3 +11,14 @@ api.interceptors.request.use((config) => {
   console.log(`[API Request] -> ${config.method?.toUpperCase()} ${config.url}`);
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => {
+    console.log(`[API Response] -> ${response.config.method?.toUpperCase()} ${response.config.url} | Status: ${response.status}`);
+    return response;
+  },
+  (error) => {
+    console.error(`[API Error] -> ${error.config?.method?.toUpperCase()} ${error.config?.url} | Status: ${error.response?.status}`);
+    return Promise.reject(error);
+  }
+);
